6c3594711d8151f8aba6543a75a319b6844809b0
luzhenxing <luzhenxing_1230@163.com>
2017-04-27 00:11:27 +0800

    commit

--- app/scripts/UEditor.js
+++ app/scripts/UEditor.js
@@ -1,22 +1,118 @@
 define(() => {
-  return {
-    editorCache: {},
-    // 创建编辑器
-    renderUEditor(dom, height, content){
-      let id = dom.id;
-      if (! this.editorCache.hasOwnProperty(id)) {
-        this.createUEditor(dom, height, content);
-      } else {
-        this.editorCache[id].setHeight(height);
-        this.editorCache[id].setShow();
-        this.editorCache[id].setContent(content);
-      }
-    },
-    createUEditor(id, height, content){
-      this.editorCache[id] = UE.getEditor(id, {
-        initialContent: content,
-        initialFrameHeight: height
+  const UEDITOR_MIN_HEIGHT = 100
+  // 编辑器类
+  function UEditor($box, content = '') {
+    this.box = $box
+    this.id = $box.data('id')
+    this.type = $box.data('type') // create & edit
+    this.wrap = $box.find('.item-ueditor-wrap')
+    this.inner = $box.find('.item-inner')
+    this.ueitor = null
+    this.content = content
+    this.init()
+  }
+
+  UEditor.prototype = {
+    // 初始化编辑器
+    init(){
+      this.wrap.html(`
+        <div id="ueditor_${this.id}" class="item-ueditor"></div>
+        <div class="ueditor-btn-group">
+          <button class="btn btn-default btn-xs hook-ueditor-cancel">取消</button>
+          <button class="btn btn-success btn-xs hook-ueditor-save" disabled>保存</button>
+        </div>
+      `)
+
+      let height = Math.max(this.box.height(), UEDITOR_MIN_HEIGHT)
+      this.ueditor = UE.getEditor(`ueditor_${this.id}`, {
+        initialFrameHeight: height,
+        initialContent: this.content
       })
+      this.bindEvent()
+      console.log(this.ueditor)
+      this.show();
+    },
+    // 绑定事件
+    bindEvent(){
+      let _this = this
+      this.wrap.on('click', '.hook-ueditor-save', () => {
+        if (this.isContentEmpty()) {
+          this.wrap.find('.hook-ueditor-save').prop('disabled', true)
+          return;
+        }
+        this.box.data('type', 'edit');
+        this.inner.html(_this.getContent())
+        this.hide()
+        $(this).trigger('save')
+      })
+        .on('click', '.hook-ueditor-cancel', () => {
+          this.hide();
+          if (this.box.data('type') === 'create'){
+            // 如果创建时取消则销毁段落
+            this.box.remove();
+            $(this).trigger('destroy')
+          }
+        })
+
+
+
+      this.ueditor.addListener('contentChange', () => {
+        this.wrap.find('.hook-ueditor-save').prop('disabled', this.isContentEmpty())
+      })
+    },
+    // 显示编辑器
+    show(){
+      $('.item-button-group .btn').prop('disabled', true);
+      this.wrap.show()
+      this.inner.hide()
+    },
+    // 隐藏编辑器
+    hide(){
+      $('.item-button-group .btn').prop('disabled', false);
+      this.wrap.hide()
+      this.inner.show()
+    },
+    // 保存内容
+    save(){},
+    // 取消保存
+    cancel(){},
+    // 获取内容
+    getContent(){
+      return this.ueditor.getContent()
+    },
+    // 设置内容
+    setContent(cont){
+      this.ueditor.setContent(cont)
+    },
+    // 内容是否为空
+    isContentEmpty(){
+      return this.ueditor.getContentTxt().trim() === ''
+    },
+    // 设置高度
+    setHeight(h){
+      this.ueditor.setHeight(h)
     }
   }
-});
+  return UEditor
+  // return {
+  //   editorCache: {},
+  //   // 创建编辑器
+  //   renderUEditor(dom, height, content){
+  //     let id = dom.id;
+  //     if (! this.editorCache.hasOwnProperty(id)) {
+  //       this.createUEditor(id, height, content);
+  //     } else {
+  //       this.editorCache[id].setHeight(height);
+  //       this.editorCache[id].setShow();
+  //       this.editorCache[id].setContent(content);
+  //     }
+  //   },
+  //   createUEditor(id, height, content){
+  //     console.log(id, height, content)
+  //     this.editorCache[id] = UE.getEditor(id, {
+  //       initialContent: content,
+  //       initialFrameHeight: height
+  //     })
+  //   }
+  // }
+})
--- app/scripts/item-ueditor.js
+++ app/scripts/item-ueditor.js
@@ -1,11 +1,14 @@
 define(['scripts/UEditor'], (UEditor) => {
-  const UEDITOR_MIN_HEIGHT = 100;
+  // 当前段落id集合
+  let itemIds = {}
+  let checkedItem = {}
 
   function ItemUeditor() {
     this.$layer = null
   }
 
   ItemUeditor.prototype = {
+    // 获取编辑弹层
     getLayer(){
       if (!this.$layer) {
         let tpl = template('tpl-item-ueditor-layer', {})
@@ -15,47 +18,117 @@ define(['scripts/UEditor'], (UEditor) => {
         this.bindEvent()
       }
     },
+    // 绑定事件
     bindEvent(){
-      // 取消
-      this.$layer.on('click', '#ueditor-btn-cancel', () => {
-        this.hide()
+      // 添加段落
+      this.$layer.on('click', '#ueditor-btn-add', () => {
+        this.addItem()
       })
+      // 删除段落
+        .on('click', '#ueditor-btn-delete', () => {
+          this.deleteItem()
+        })
+        // 导入文档
+        .on('click', '#ueditor-btn-import', () => {
+          this.importItem()
+        })
+        // 取消
+        .on('click', '#ueditor-btn-cancel', () => {
+          this.hide()
+        })
+        // 点击内容显示编辑器
+        .on('click', '.item-inner', function () {
+          let $obj = $(this).closest('li'),
+            editor = $obj.data('editor')
+          if (editor === undefined) {
+            editor = new UEditor($obj, $(this).html())
+            $obj.data('editor', editor)
+            $(editor).on('destroy', () => {
+              $obj.data('editor', undefined)
+            })
+          } else {
+            editor.setContent($(this).html())
+            editor.show()
+          }
+          // this.setContent(this.inner.html())
+          // this.show()
+        })
+        // checkbox
         .on('change', '.hook-item-checkbox', function () {
-          let $label = $(this).closest('label.checkbox')
+          let $label = $(this).closest('label.checkbox'),
+            $li = $label.closest('li'),
+            id = $li.data('id')
           if (this.checked) {
             $label.addClass('checked')
+            checkedItem[id] = $li
           } else {
             $label.removeClass('checked')
+            delete checkedItem[id]
           }
         })
     },
+    // 显示弹层
     show(){
       this.getLayer()
-
       this.$layer.show().addClass('fadeIn')
     },
+    // 隐藏弹层
     hide(){
       this.$layer.hide()
     },
+    // 创建item
     createItem(){
-      let $li = $(this.getItemListTpl())
-      this.$layer.find('.loading-wrap').remove().end()
-        .find('.item-list').append($li)
-      this.renderUEditor($li);
+      let $li = $(this.getItemListTpl({id: Date.now(), type: 'create'}))
+      this.$layer.find('.item-list').prepend($li)
+      return $li
     },
-    getItemListTpl(){
-      return template('tpl-item-list',
-        {id: Date.now(), type: 'edit'})
+    // 添加段落
+    addItem(){
+      let $obj = this.createItem()
+      let editor = new UEditor($obj)
+      $obj.data('editor', editor)
+      $(editor).on('destroy', () => {
+        $obj.data('editor', null)
+      })
     },
+    // 获取段落dom模版
+    getItemListTpl(data){
+      return template('tpl-item-list', data)
+    },
+    // 渲染一个编辑器
     renderUEditor($li){
-      let height = Math.max($li.height(), UEDITOR_MIN_HEIGHT);
-      let ueditorDom = $li.find('.item-ueditor').get(0);
-      let content = '';
-      if ($li.data('type') == 'edit'){
-        content = $li.find('.item-inner').html();
+      console.log($li)
+      let height = Math.max($li.height(), UEDITOR_MIN_HEIGHT)
+      let ueditorDom = $li.find('.item-ueditor').get(0)
+      let content = ''
+      if ($li.data('type') === 'edit') {
+        content = $li.find('.item-inner').html()
       }
-      UEditor.renderUEditor(ueditorDom, height, content);
-      $li.find('.item-inner').hide();
+      UEditor.renderUEditor(ueditorDom, height, content)
+      $li.find('.item-inner').hide()
+    },
+    disabledAddBtn(id, type) {
+      $(`#${id}`).prop('disabled', type)
+    },
+    deleteItem(){
+      for (let id in checkedItem) {
+        $(`#item_${id}`).data('editor', null)
+        $(`#item_${id}`).remove()
+        delete checkedItem[id]
+      }
+    },
+    importItem(){
+      $.getJSON('json/item-list.json').then(result => {
+        console.log(result.data)
+        this.renderItems(result.data)
+      })
+    },
+    renderItems(data){
+      let tpl = ''
+      data.forEach(item => {
+        tpl += this.getItemListTpl($.extend(item, {type: 'edit'}))
+      })
+      this.$layer.find('.item-list').html(tpl)
     }
   }
 
@@ -65,7 +138,7 @@ define(['scripts/UEditor'], (UEditor) => {
     setOptions(ops = {}){
       $.extend(itemUeditor, ops)
     },
-    show(command = 'create'){
+    show(command = ''){
       itemUeditor.show()
       switch (command) {
         case 'create':
